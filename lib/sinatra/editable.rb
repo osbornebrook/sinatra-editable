cb6c466f06c9ce55d2257f6af156dbad974cb1b7
Jonathan Davies <jonnie@obdev.co.uk>
2012-11-05 16:35:29 +0000

    addumed html is utf8

--- lib/sinatra/editable.rb
+++ lib/sinatra/editable.rb
@@ -4,10 +4,10 @@ module Sinatra
   module Editable
 
     module Helpers
-      def editable(item)
+      def editable(item, encoding = "UTF-8")
         path = "#{settings.root}/#{settings.editable_dir}/#{item.to_s}.html"
         if editable_exist?(item)
-          File.read(path)
+          File.read(path, encoding)
         end
       end
 
@@ -44,24 +44,24 @@ module Sinatra
         else
           raise "Bad templater option"
         end
-       
+
         splat = params[:splat].shift.split('/')
         file_basename = splat.pop.gsub(/\/$/,'')
-                
-        dir = "#{settings.root}/#{settings.editable_dir}/#{splat.join('/')}" 
+
+        dir = "#{settings.root}/#{settings.editable_dir}/#{splat.join('/')}"
 
         html_path = "#{dir}/#{file_basename}.html"
         template_path = "#{dir}/#{file_basename}.#{settings.editable_templater}"
-        
+
         FileUtils.mkdir_p dir unless File.exist?(dir)
- 
+
         if new_template
            File.open(template_path,'w') do |f|
             f.print(new_template)
             f.close
           end
         end
-        
+
         File.open(html_path, 'w') do |f|
           f.print(new_html)
           f.close
